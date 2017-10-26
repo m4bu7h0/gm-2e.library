fe0309ce107486be4717e81f7b1f7d6b40c97e5b
Sibongeleni Mdlalose <sibongelenim@clubtech.co.za>
2017-10-26 10:55:10 +0200

    Add project files.

---
+++ gm-2e.library/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gm_2e.library.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
